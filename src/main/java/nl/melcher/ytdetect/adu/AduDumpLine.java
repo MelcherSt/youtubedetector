30c6e6f49c18337a43aa2369b530805822fec94d
Melcher <melcherst@outlook.com>
2017-05-30 11:22:49 +0200

    Add adudump parser

---
+++ src/main/java/nl/melcher/ytdetect/adu/AduDumpLine.java
@@ -0,0 +1,60 @@
+package nl.melcher.ytdetect.adu;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * Partly representation of a single line of `adudump` output.
+ */
+@AllArgsConstructor
+public class AduDumpLine {
+
+	private InferredType type;
+	private String timestamp;
+	private String addrOne;
+	private String addrTwo;
+	private int size = 0;
+	private Direction direction;
+
+	public String getFromAddress() {
+		if(direction == Direction.INCOMING) {
+			return addrTwo;
+		} else if(direction == Direction.OUTGOING) {
+			return addrOne;
+		}
+		return "";
+	}
+
+	public String getToAddress() {
+		if(direction == Direction.INCOMING) {
+			return addrOne;
+		} else if(direction == Direction.OUTGOING) {
+			return addrTwo;
+		}
+		return "";
+	}
+
+	public enum InferredType {
+		SYN, RTT, SEQ, ADU, CNC, INC, UNKNOWN, END
+	}
+
+	public enum Direction {
+		INCOMING, OUTGOING, UNKNOWN;
+
+		public static Direction fromString(String sign) {
+			switch (sign) {
+				case "<1":
+					return Direction.INCOMING;
+				case ">1":
+					return Direction.OUTGOING;
+				default:
+					return Direction.UNKNOWN;
+			}
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "AduDumpLine[Type=" + type + ",Timestamp=" + timestamp + ",AddrOne="
+				+ addrOne + ",AddrTwo=" + addrTwo + ",Direction=" + direction + ",Size=" + size + "]";
+	}
+}
